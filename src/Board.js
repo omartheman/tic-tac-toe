596c558914ac489cc3a21f9dd4524c9d8fc93f24
Omar Shishani <omarshishanischool@gmail.com>
2020-11-23 18:16:34 -0800

    Successfully add time travel

--- src/Board.js
+++ src/Board.js
@@ -1,68 +1,39 @@
 import React from 'react';
 import Square from './Square';
 
-//tell each square it's state
-class Board extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-      history: []
-    }
-    this.handleClick = this.handleClick.bind(this);
-  }
-  renderSquare(i) {
+//Rerender Board when a phase is clicked. 
+
+function Board(props){
+  const {squares, handleClick} = props; 
+
+  function renderSquare(i){
     return (
-      <Square 
-        value={this.state.squares[i]} 
-        onClick = {() => {this.handleClick(i)}}
-      />);
-  }
-  render() {
-    const squares = this.state.squares.slice();
-    let status = calculateWinner(squares) ? `Winner is: ${calculateWinner(squares)}` : `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+      <Square
+        value={squares[i]}
+        onClick = {() => {handleClick(i)}}
+      />
     );
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
+    
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
 }
 
 export default Board;
--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import Board from './Board';
 
+// STOP: Display different states of game on click. 
+// 1. List each phase of game next to board
+// 2. When you click on one of the phases, make board go back to that phase.
+//    a. If you click on a square after going back to a phase "X", delete all phases that came after phase you clicked on, and continue game from phase "X"
+// 3. Create a button for each phase. Give these buttons a unique id, so you can click on them and go back to a certain phase. 
+// 4. When you click on a phase button, set the state of the board to be that phase.
+//    a. The selected phase has id "i", which corresponds to the index of "this.state.history" that matches the phase. 
+//    b. When you click on phase 1, use this.state.history[i].
 class Game extends React.Component {
   constructor(props){
     super(props);
@@ -9,17 +17,11 @@ class Game extends React.Component {
       xIsNext: true,
       history: []
     }
+    this.handleClick = this.handleClick.bind(this);
   }
-  //To Do to lift state:
-  // 1. Move relevant functions if necessary
-  // 2. Pass state as props to relevant components 
-  // 3. Change "this.state...." to "this.props..." in Board Component
-  
   handleClick(i){
     const squares = this.state.squares.slice(); 
     const history = this.state.history.slice();
-    //state needs to be set so that Game gets info about "squares"
-    //since handleClick needs to be used to set the state of "squares", handleClick needs to be lifted. 
     if (calculateWinner(squares) || squares[i]){return}
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
@@ -29,19 +31,58 @@ class Game extends React.Component {
     }, ()=>{console.log('history',this.state.history)})
   }
   render() {
+    const {squares, history} = this.state;
+    let status = calculateWinner(squares) ? `Winner is: ${calculateWinner(squares)}` : `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    //add onClick to each phase, so that you can set state to correct history point.
+    const phases =  history.map((e, i) => (
+      <li 
+        key={i} 
+        id={i}
+        onClick={() => {
+          this.setState({squares: history[i].squares})
+        }}
+      >
+        <button> Go to phase {i+1}</button>
+      </li>
+    ))
+
     return (
       <div className="game">
         <div className="game-board">
-          <h2>Heyder Britt, wanna play sum tiktakto?</h2>
-          <Board />
+          <Board 
+            squares={this.state.squares}
+            handleClick={this.handleClick}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div className="status">{status}</div>
+          <ol>{/* TODO Add phases here */}
+            {phases}
+          </ol>
         </div>
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Game;
